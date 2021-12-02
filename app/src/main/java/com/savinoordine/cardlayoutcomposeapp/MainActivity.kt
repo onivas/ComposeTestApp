b706f6f35a93a39fd39d0f79ac2c8262296ba09d
Ordine Savino <savino.ordine@pirelli.com>
2021-12-02 21:56:27 +0100

    structure with a custom color

---
+++ app/src/main/java/com/savinoordine/cardlayoutcomposeapp/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.savinoordine.cardlayoutcomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.savinoordine.cardlayoutcomposeapp.ui.theme.LightGreen
+import com.savinoordine.cardlayoutcomposeapp.ui.theme.MyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.LightGray
+    ) {
+        ProfileCard()
+    }
+}
+
+@Composable
+fun ProfileCard() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top)
+            .padding(16.dp),
+        elevation = 8.dp,
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            ProfilePicture()
+            ProfileContent()
+        }
+    }
+}
+
+@Composable
+fun ProfileContent() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(
+            text = "Savino Taaac",
+            style = MaterialTheme.typography.h5
+        )
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(
+                text = "Active now",
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
+
+@Composable
+fun ProfilePicture() {
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(
+            width = 2.dp,
+            color = MaterialTheme.colors.LightGreen,
+        ),
+        modifier = Modifier
+            .padding(16.dp),
+        elevation = 6.dp,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "",
+            modifier = Modifier.size(72.dp),
+            contentScale = ContentScale.Crop,
+        )
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyTheme {
+        MainScreen()
+    }
+}
---
+++ app/src/main/java/com/savinoordine/cardlayoutcomposeapp/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.savinoordine.cardlayoutcomposeapp.ui.theme
+
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+private val LightGreen200 = Color(0xFF80C84F)
+
+val Colors.LightGreen: Color
+    @Composable
+    get() = LightGreen200
